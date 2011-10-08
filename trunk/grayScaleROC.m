4321da48933b8393e5b165151b151d502af3a13d
VLAD.LEP <VLAD.LEP@84f39a60-67e4-10de-bc20-1410b4be1a34>
2011-10-08 18:51:01 +0000

    general functions for ROC  + roc for gray scale

---
+++ trunk/grayScaleROC.m
@@ -0,0 +1,15 @@
+function [ ] = grayScaleROC(  )
+%GRAYSCALEROC Summary of this function goes here
+%   Detailed explanation goes here
+load mlpr_data\data_intensity_25x50.mat;
+
+pedTrain = ped_train_int_25x50(:, 2:1251); 
+nonPedTrain = garb_train_int_25x50(:, 2:1251);
+
+
+trainData = [pedTrain; nonPedTrain];
+
+nrOfDimensions  = rocCompute(trainData, 0.99)
+
+end
+
