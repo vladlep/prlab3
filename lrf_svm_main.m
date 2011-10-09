b10619c89937b467dc33ef622edff5690a95033e
VLAD.LEP <VLAD.LEP@84f39a60-67e4-10de-bc20-1410b4be1a34>
2011-10-09 11:20:34 +0000

    implemented the test phase

--- lrf_svm_main.m
+++ lrf_svm_main.m
@@ -6,16 +6,38 @@ load mlpr_data\data_lrf.mat;
     pedData= ped_train_lrf(:,2:321);
     nonPedData = garb_train_lrf(:,2:321);
     trainData = [pedData; nonPedData];
-    size(trainData)
+    size(trainData);
     
-    rocCompute(trainData,0.99)
+%     rocCompute(trainData,0.99)
    
+    %training phase
     global  X;% defined for the svm algorithm 
     X = trainData; 
     pedLabels =  ped_train_lrf(:,1);
     nonPedLabels =  garb_train_lrf(:,1);
     labels = [pedLabels; nonPedLabels];
-   [SOL,B] = primal_svm(1,labels,0.5)
-   
+    [SOL,B] = primal_svm(1,labels,0.5);
+    
+    % test phase   
+    pedTest = ped_test_lrf(:,2:321);
+    nonPedTest = garb_test_lrf(:,2:321);
+    errorPed = 0;
+    for i=1 : size(pedTest,1)
+    if pedTest(i,:) *SOL + B < 0
+        errorPed = errorPed + 1;
+    end
+    end
+    
+    errorNonPed =0;
+    for i=1 : size(pedTest,1)
+    if nonPedTest(i,:) * SOL + B > 0
+        errorNonPed = errorNonPed + 1;
+    end
+    end
+    fprintf('error pedestrians classification ' );
+    errorPed 
+    
+    fprintf('error non - pedestrians classification ' );
+    errorNonPed
 end
 
