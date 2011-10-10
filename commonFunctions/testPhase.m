3c81a47856f19617b82d49688869f83123da90d2
VLAD.LEP <VLAD.LEP@84f39a60-67e4-10de-bc20-1410b4be1a34>
2011-10-10 20:14:14 +0000

    - general testing pahse for the SVM alg testPhase.m

---
+++ commonFunctions/testPhase.m
@@ -0,0 +1,28 @@
+function [ ] = testPhase( threshold ,SOL, B , pedTest, nonPedTest )
+%TESTPHASE Function used to test the classifer. 
+%   B and SOL are the parametrs learned from by the SVM on the training
+%   data
+
+%     load mlpr_data\data_lrf.mat;
+%     pedTest = ped_test_lrf(:,2:321);
+%     nonPedTest = garb_test_lrf(:,2:321);
+    errorPed = 0;
+    for i=1 : size(pedTest,1)
+    if pedTest(i,:) *SOL + B < threshold
+        errorPed = errorPed + 1;
+    end
+    end
+    
+    errorNonPed =0;
+    for i=1 : size(nonPedTest,1)
+    if nonPedTest(i,:) * SOL + B > threshold
+        errorNonPed = errorNonPed + 1;
+    end
+    end
+    fprintf('percent error pedestrians classification ' );
+    errorPed/ size(nonPedTest,1)
+    
+    fprintf('error non - pedestrians classification ' );
+    errorNonPed/size(pedTest,1)
+end
+
